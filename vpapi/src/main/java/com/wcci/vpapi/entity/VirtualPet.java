69d999a3aa6ba376ffd9ec24b675a991b579f648
klocke1028 <k.locke1028@gmail.com>
2024-07-05 13:57:43 -0400

    Maybe working now?

--- vpapi/src/main/java/com/wcci/vpapi/entity/VirtualPet.java
+++ vpapi/src/main/java/com/wcci/vpapi/entity/VirtualPet.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,7 +36,7 @@ public class VirtualPet {
     @Column(name = "Happiness")
     private Integer happiness;
 
-    
+    @ManyToMany(fetch = FetchType.LAZY)
     private List<VirtualPet> virtualPets;
 
     public VirtualPet() {
