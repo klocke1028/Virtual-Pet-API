f1859c00a6145d726efd15d8792bd321686ceff7
klocke1028 <k.locke1028@gmail.com>
2024-07-04 23:35:04 -0400

    Hate VS Code

--- vpapi/src/main/java/com/wcci/vpapi/repository/VirtualPetsRepository.java
+++ vpapi/src/main/java/com/wcci/vpapi/repository/VirtualPetsRepository.java
@@ -1,5 +1,11 @@
 package com.wcci.vpapi.repository;
 
-public interface VirtualPetsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wcci.vpapi.entity.VirtualPets;
+
+@Repository
+public interface VirtualPetsRepository extends JpaRepository<VirtualPets, Long>{
 
 }
